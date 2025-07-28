d26a3179124b5f7177be977d37a04810191eec0a
lone-DO <citizin@outlook.com>
2025-07-28 17:21:51 +0200

    Todos - Allow mock deleting

--- src/app/assets/pipes/filter-todos-pipe.ts
+++ src/app/assets/pipes/filter-todos-pipe.ts
@@ -7,9 +7,12 @@ import { iTodo } from '@/assets/services/todos';
 })
 export class FilterTodosPipe implements PipeTransform {
   transform(value: iTodo[], searchText: string): iTodo[] {
-    if (!searchText) return value;
-    return value.filter((item) =>
-      item.title.toLowerCase().includes(searchText.toLocaleLowerCase())
-    );
+    return value.filter((item) => {
+      if (!searchText) return !item.hidden;
+      return (
+        !item.hidden &&
+        item.title.toLowerCase().includes(searchText.toLocaleLowerCase())
+      );
+    });
   }
 }
--- src/app/components/todo-item/todo-item.ts
+++ src/app/components/todo-item/todo-item.ts
@@ -13,21 +13,24 @@ import { iTodo } from '@/assets/services/todos';
 export class TodoItem {
   todo = input.required<iTodo>();
   editing = signal(false);
-  internalTitle = signal('');
-  ngOnInit(): void {
-    this.internalTitle.set(this.todo().title);
-  }
+  _title = signal('');
+
   @Output() updated = new EventEmitter<iTodo>();
+  /** LifeCycle */
+  ngOnChanges(): void {
+    this._title.set(this.todo().title);
+  }
+  /** Methods */
   toggleEditing = () => this.editing.update((bool) => !bool);
+  removeTodo = () => this.updated.emit({ ...this.todo(), hidden: true });
   updateTodo = (type?: string) => {
     this.editing.set(false);
     if (type === 'cancel') {
-      this.internalTitle.set(this.todo().title);
+      this._title.set(this.todo().title);
     } else if (type === 'edit') {
-      this.updated.emit({ ...this.todo(), title: this.internalTitle() });
+      this.updated.emit({ ...this.todo(), title: this._title() });
     } else {
       this.updated.emit({ ...this.todo(), completed: !this.todo().completed });
     }
   };
-  removeTodo = () => this.updated.emit({ ...this.todo(), hidden: true });
 }
